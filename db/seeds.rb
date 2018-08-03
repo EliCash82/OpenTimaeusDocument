95675f34556c1feb80379a34e69002c0a6d704b7
anderson <almostanderson@gmail.com>
2018-08-02 23:02:17 -0400

    Added styling to annotations database started search functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+annotations = Annotation.create([{title: 'Atlantis'}, {text: 'Timaeus and Critias are the only 
+written records from ancient antiquity that specifically make a reference to Atlantis. Atlantis may be a completely fictional concept from Plato.'}, {source: 'Christopher Gill 1979 Platos Atlantis Story and the Birth of Fiction' }])
